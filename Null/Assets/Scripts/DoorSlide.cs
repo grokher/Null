7cc3e6d01518e925b4f0fa305a1315ac22714378
grokher <47322787+grokher@users.noreply.github.com>
2026-05-10 14:17:13 +0200

    SlideDoorScript, organized files

--- Null/Assets/Scripts/DoorSlide.cs
+++ Null/Assets/Scripts/DoorSlide.cs
@@ -5,7 +5,7 @@ public class DoorSlide : MonoBehaviour
 {
     public bool IsOpen = false;
     [SerializeField]
-    private Vector3 SlideDirection = Vector3.up;
+    private Vector3 SlideDirection = Vector3.back;
     [SerializeField]
     private float SlideAmount = 4f;
 
@@ -21,17 +21,22 @@ public class DoorSlide : MonoBehaviour
         StartPos = transform.position;
     }
 
-    public void Open()
+    public void Open(Vector3 UserPosition)
     {
-        if (IsOpen)
+        if (!IsOpen)
         {
+            if(AnimationCoroutine != null)
+            {
+                StopCoroutine(AnimationCoroutine);
+                
+            }
             AnimationCoroutine = StartCoroutine(DoSlidingOpen());
         }
     }
 
     private IEnumerator DoSlidingOpen()
     {
-        Vector3 endPosition = StartPos+ SlideAmount * SlideDirection;
+        Vector3 endPosition = StartPos + SlideAmount * SlideDirection;
         Vector3 startPosition = transform.position;
 
         float time = 0;
@@ -46,8 +51,13 @@ public class DoorSlide : MonoBehaviour
 
     public void Close()
     {
-        if (!IsOpen)
+        if (IsOpen)
         {
+            if(AnimationCoroutine != null)
+            {
+                StopCoroutine (AnimationCoroutine);
+            }
+
             AnimationCoroutine = StartCoroutine(DoSlidingClosed());
         }
     }
